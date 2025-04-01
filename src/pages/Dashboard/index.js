fa6b777b0998d43b3a889892ae88b82d72ea493f
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-04-01 15:08:11 -0300

    Criando o dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,19 +1,55 @@
-import { FiMessageCircle } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { FiEdit2, FiMessageCircle, FiSearch } from "react-icons/fi";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
+import "./dashboard.css";
 
-export default function Dashboard(){
-    return(
-        <div>
-            <Header />
-        
-            <div className="content">
-                   <Title name="Consulta OS">
-                       <FiMessageCircle size={25} />
-                   </Title>
-            </div>
+export default function Dashboard() {
+  return (
+    <div>
+      <Header />
 
-            <h1>Teste</h1>
-        </div>
-    )
+      <div className="content">
+        <Title name="Minhas OS">
+          <FiMessageCircle size={25} />
+        </Title>
+
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">id</th>
+              <th scope="col">Cliente</th>
+              <th scope="col">Assunto</th>
+              <th scope="col">Status</th>
+              <th scope="col">Cadastrando em</th>
+              <th scope="col">#</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td data-label="Id">1</td>
+              <td data-label="Cliente">João</td>
+              <td data-label="Assunto">
+                {" "}
+                {"Impressora - Falha na impressão".length > 10
+                  ? "Impressora - Falha na impressão".slice(0, 10) + "..."
+                  : "Impressora - Falha na impressão"}{" "}
+              </td>
+              <td data-label="Status">
+                {" "}
+                Aberto
+                <span className="badge">Aberto</span>
+              </td>
+              <td data-label="Cadastrado">10/10/2024</td>
+              <td data-label="#">
+                <button className="action" style={{ backgroundColor: '#3583f6' }} >
+                  <FiSearch color="#FFF"  size={17} />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
